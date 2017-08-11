5881a46e4189b58e54ab80dc4679662e8b72f83f
J F Dama <james.f.dama@gmail.com>
2017-08-11 15:39:03 -0400

    Converted to printing Nv rather than (C + N)v.

--- src/engine.cpp
+++ src/engine.cpp
@@ -298,10 +298,10 @@ void Engine::matrixSolver()
     // Check CNV vector
     bool check_cnv_vector = true;
     if (check_cnv_vector) {
-        checkmap << "Printing (C + N)v. Step: " << fg_atoms->currentStep << ", N_CG: "<< cg_num << std::endl;
+        checkmap << "Printing Nv. Step: " << fg_atoms->currentStep << ", N_CG: "<< cg_num << std::endl;
         for (int i=0; i<cg_num; i++)
         {
-            checkmap << flat_CNv[i] << '\t' << flat_CNv[i + cg_num] << '\t' << flat_CNv[i + 2 * cg_num] << std::endl;
+            checkmap << flat_CNv[i] - cg_sites->VMAP[i] << '\t' << flat_CNv[i + cg_num] - cg_sites->VMAP[i + cg_num] << '\t' << flat_CNv[i + 2 * cg_num] - cg_sites->VMAP[i + 2 * cg_num] << std::endl;
         }
     }
     cblas_dgemv(order, transa, m, m, alpha, A, m, flat_CNv, incx, beta, V_CG, incy);
