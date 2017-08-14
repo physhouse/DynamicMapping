77219c280676a905966ddd1671c1228934e8479e
J F Dama <james.f.dama@gmail.com>
2017-08-14 14:25:25 -0400

    Simplified the N matrix element calculation using pre-computed w_sum and gradient.

--- src/vector_CNv.cpp
+++ src/vector_CNv.cpp
@@ -41,6 +41,7 @@ void Vector_CNv::generate_CNv()
     double ***  dw     = matrix_C->dw;
     double   **W      = matrix_C->W;
     double    *w_sum  = matrix_C->w_sum;
+    double    **dw_sum  = matrix_C->dw_sum;
     double *v = fg_atoms->v;
 
     for (int I = 0; I < cg_num; I++)
@@ -51,69 +52,34 @@ void Vector_CNv::generate_CNv()
         }
     }
 
-    // To compute (C + N) * v, we start by looping over the fine-grained j,
-    // then choose a neighboring CG site J, then a second neighboring site I,
-    // and compute the influence of the Jj ownership relationship on each I
-    // according to
-    // (Nv)_IJj = R_Ij * c_Ij * (\delta_IJ - f_Jj) * -(d log w(R_Jj) / d R_Jj \cdot v_j)
-    // First one gets v_j into cache, then calculates -(d log w(R_Jj) / d R_Jj \cdot v_j),
-    // then calculates -R_Ij * c_Ij * (\delta_IJ - f_Jj) * (d log w(R_Jj) / d R_Jj \cdot v_j).
-    // Cv is accumulated along the way as well.
-    // See Eq. 17 of Overlap_Matrix_Physics_III.pdf
-
-    //for (int j = 0; j < cg_sites->cg_num; j++) {
     for (int j = 0; j < fg_atoms->fg_num; j++) {
-        // Access v_j
         double vj[3];
         for (int jdim = 0; jdim < 3; jdim++) 
         {
             vj[jdim] = v[jdim * fg_num + j];
         }
-        // Calculate -(d log w(R_Jj) / d R_Jj \cdot v_j) for each neighbor J
-        // This is -(1 / w_Jj) * (sum_jdim dw_Jjjdim * v_jjdim) 
+
         for (int cgneigh = 0; cgneigh < neighbor->numFgNeighbors[j]; cgneigh++)
         {
-            int J = neighbor->fgList[j][cgneigh];
-            double vj_dot_logprox_Jj = 0.0;
-            for (int jdim = 0; jdim < 3; jdim++) 
+            int I = neighbor->fgList[j][cgneigh];
+            double N_Ij_scalar = 0;
+            // 
+            for (int jdim = 0; jdim < 3; jdim++)
             {
-                vj_dot_logprox_Jj += dw[J][j][jdim] * vj[jdim];
+                N_Ij_scalar += C[I][j] * (dw[I][j][jdim] / W[I][j] - dw_sum[j][jdim] / w_sum[j]) * vj[jdim];
             }
-            vj_dot_logprox_Jj /= W[J][j];
-
-            // Prepare f_Jj also.
-            double f_Jj = W[J][j] / w_sum[j];
-            
-            // Calculate -R_Ij * c_Ij * (\delta_IJ - f_Jj) * (d log w(R_Jj) / d R_Jj \cdot v_j)
-            // for each I.
-            for (int cgneigh2 = 0; cgneigh2 < neighbor->numFgNeighbors[j]; cgneigh2++) 
+            for (int Idim = 0; Idim < 3; Idim++)
             {
-                int I = neighbor->fgList[j][cgneigh2];
-
-                if (I == J) {
-                    // Add the contribution from Nv. Delta_IJ = 1.
-                    double N_IJj_scalar = C[I][j] * (1 - f_Jj) * vj_dot_logprox_Jj;
-                    for (int Idim = 0; Idim < 3; Idim++) {
-                        double coord = r[j][Idim];
-                        wrap_coord_relative(coord, R[I][Idim], fg_atoms->L);
-                        CNv[Idim][I] +=  (R[I][Idim] - coord) * N_IJj_scalar;
-                    }
-                    // Add the contribution from Cv
-                    for (int Idim = 0; Idim < 3; Idim++) {
-                        CNv[Idim][I] += C[I][j] * vj[Idim];
-                    }
-                } else {
-                    // Add the contribution from Nv. Delta_IJ = 0.
-                    double N_IJj_scalar = - C[I][j] * f_Jj * vj_dot_logprox_Jj;
-                    for (int Idim = 0; Idim < 3; Idim++) {
-                        double coord = r[j][Idim];
-                        wrap_coord_relative(coord, R[I][Idim], fg_atoms->L);
-                        CNv[Idim][I] +=  (R[I][Idim] - coord) * N_IJj_scalar;
-                    }
-                }
+                // Add the Cv vector term for this Ij pair.
+                CNv[Idim][I] += C[I][j] * vj[Idim];
+                // Add the Nv vector term for this Ij pair.
+                double coord = r[j][Idim];
+                wrap_coord_relative(coord, R[I][Idim], fg_atoms->L);
+                CNv[Idim][I] +=  (R[I][Idim] - coord) * N_Ij_scalar;
             }
         }
     }
+
 }
 
 void Vector_CNv::compute()
